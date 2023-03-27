b9222e972412a55e51e505f7dcb08690ec8d6ccc
ristr1ssa <roba2005avagyan@gmail.com>
2023-03-27 20:48:48 +0300

    some new

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,10 +1,3 @@
-function get_hash() {
-    const currentUrl = window.location.href;
-    hash = currentUrl.split("p/")[1];
-    link = `https://t.me/dmmebaby_bot?start=${hash}`
-    return window.open(link);
-};
-
 function open_hash() {
     const hash = document.getElementById('input_hash').value;
 
